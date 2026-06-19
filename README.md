8edbc9f9aec7f2e30191c2b32af04b2db391d387
mohan <vmohan619@gmail.com>
2026-06-19 15:24:29 +0000

    updating folder structure

---
+++ README.md
@@ -0,0 +1,4 @@
+Environment Branch Name and Configuration file name are same
+development develop 
+staging stage
+production prod
---
+++ environments/develop/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket       = "mohan-terraform-tfstate-file"
+    key          = "terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+}
